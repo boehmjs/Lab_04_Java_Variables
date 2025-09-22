a7e527fca3414bd1705c38526ce9235813696873
boehmjs <boehmjs@mail.uc.edu>
2025-09-22 16:29:14 -0400

    2nd Commit after declaring variables and creating assignment statements

---
+++ Lab_04_Java_Variables/src/Main.java
@@ -0,0 +1,33 @@
+
+void main() {
+    int intOperandA = 24;
+    int intOperandB = 5;
+    int intSum = 7;
+    int intProduct = 12;
+    int intDifference = 8;
+    int intQuotient = 19;
+    int intModulo = 3;
+    double doubleOperandA = 3.50;
+    double doubleOperandB = 4.50;
+    double doubleSum = 5.50;
+    double doubleProduct = 6.50;
+    double doubleDifference = 7.50;
+    double doubleQuotient = 8.50;
+
+
+    intSum = intOperandA + intOperandB;
+    intProduct = intOperandA * intOperandB;
+    intDifference = intOperandA - intOperandB;
+    intQuotient = intOperandA / intOperandB;
+
+
+    System.out.println("The sum using ints of " + intOperandA + " " + intOperandB + " is " + intSum);
+    System.out.println("The product of using ints of " + intOperandA + " " + intOperandB + " is " + intProduct);
+    System.out.println("The difference using ints of " + intOperandA + " " + intOperandB + " is " + intDifference);
+}
+
+
+
+
+
+
